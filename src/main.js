8e86e6e5dcb5d5cae9176337015c7f6cf55eb887
驭舟 <li.lwd@alibaba-inc.com>
2020-08-09 20:09:42 +0800

    feat: 🎸 接入岳鹰监控

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import store from './store'
 
 import './permission'
 
+// 接入监控
+
+import WpkReporter from 'wpk-reporter' // 导入基础sdk
+
+const __wpk = new WpkReporter({
+  bid: 'h9raxk5p-i1mcdo9n', // 新建应用时确定
+  plugins: []
+})
+
+__wpk.installAll() // 初始化sdk 必须调用
+
 Vue.config.productionTip = false
 
 new Vue({
